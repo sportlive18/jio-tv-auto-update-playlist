849f77c0df082f9cfd3084e085de55c205e23829
SAYAN <spal75084@gmail.com>
2026-09-12 10:25:25 +0530

    Update jtv.py

--- jtv.py
+++ jtv.py
@@ -10,8 +10,8 @@ from datetime import datetime
 from urllib.parse import urlparse, urlunparse
 
 CHANNELS_URL = "https://raw.githubusercontent.com/qwerty180506/json/refs/heads/main/Geoplus.json"
-COOKIE_URL = "https://raw.githubusercontent.com/qwerty180506/json/refs/heads/main/biscuit.json"
-SPORTS_COOKIE_URL = "https://raw.githubusercontent.com/qwerty180506/json/refs/heads/main/sportsbiscuit.json"
+COOKIE_URL = "https://allinonereborn2.online/jstrweb2/cookies.json"
+SPORTS_COOKIE_URL = "https://allinonereborn2.online/jtv-fetch/jstarcookie/cookie.json"
 
 M3U_FILE = "jtv.m3u"
 JSON_FILE = "jtv.json"
